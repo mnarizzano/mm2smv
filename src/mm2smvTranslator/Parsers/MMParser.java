3575fe39e631685a4cad5d41dd1cf933eb8a50dd
mnarizzano <mox@XP12-ASE.localdomain>
2022-05-13 00:14:21 +0200

    Prova close #5

---
+++ src/mm2smvTranslator/Parsers/MMParser.java
@@ -0,0 +1,14 @@
+/**
+ * This file is distribuited under the licence XXX
+ */
+package mm2smvTranslator.Parsers;
+
+/**
+ * The Class MMParser read a file written in a Graphviz dot format and construct an FSM machine.
+ * @author Massimo Narizzano
+ * @version 1.0
+ * @since 13/05/2022
+ */
+class MMParser {
+    
+};
